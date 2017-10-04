721a90908718f75f2e37a362e8f9c120c07a047c
Diljeet Singh <jeetds1994@gmail.com>
2017-10-04 18:58:53 -0400

    added instruction to search field

--- src/Search.js
+++ src/Search.js
@@ -5,6 +5,7 @@ import { Input} from 'semantic-ui-react'
 const Search = ({searchField, updateSearch, submitForm}) => {
   return(
     <form onSubmit={submitForm}>
+      <h2>Enter City:</h2>
       <Input type="text" value={searchField} onChange={updateSearch} placeholder="Enter City..."/>
     </form>
   )
